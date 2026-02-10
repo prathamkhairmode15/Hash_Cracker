267a048fe49ed06e9f53e543fcae6a78bb9cd996
Pratham Sachin Khairmode <prathamkhairmode15@gmail.com>
2026-02-10 10:46:10 +0530

    Added backend

--- backend/app.py
+++ backend/app.py
@@ -23,4 +23,6 @@ def run_dictionary_attack():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
